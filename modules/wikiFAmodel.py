230477ff91bc5dc2abec7c3478f8dd4af06c2baa
satabie <0426193@gmail.com>
2023-01-12 01:21:40 +0900

    create: model template

---
+++ modules/wikiFAmodel.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class WikiFAmodel:
+    def __init__(self, input_size=100, output_size=100, weight_decay=0):
+        self.input_size=input_size
+        self.output_size=output_size
+        self.weight_decay = weight_decay
+
+    def predict(self, x):
+        return
+
+    def loss(self, x, t):
+        return
+
+    def similarity(self, x, t):
+        return
+
+    def gradient(self, x, t):
+        return
+
